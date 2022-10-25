9866331252775f7d65331908a49514146b9e976f
soyoung.kim <guana97@naver.com>
2022-10-25 22:25:20 +0900

    020_ 몫과 나머지

--- soyoung/020_몫과_나머지.js
+++ soyoung/020_몫과_나머지.js
@@ -1,6 +1,7 @@
 //문제 : 공백으로 구분하여 두 숫자가 주어집니다.
 //두번째 숫자로 첫번째 숫자를 나누었을 때 그 몫과 나머지를 공백으로 구분하여 출력하세요.
 
+//숫자 두 개를 입력받고 공백을 기준으로 나누기
 const n = prompt('숫자를 두 개 입력하세요.').split(' ');
 
 //몫 구하기
@@ -9,4 +10,5 @@ const result = Math.floor(parseInt(n[0], 10) / parseInt(n[1], 10));
 //나머지 구하기
 const left = parseInt(n[0], 10) % parseInt(n[1], 10);
 
+//출력
 console.log(result, left);
